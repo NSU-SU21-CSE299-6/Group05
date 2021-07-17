aa7a6de30913a096a9d41211833aaac4bf8ea1ef
Arkay Saha <86014000+raidatalukderr@users.noreply.github.com>
2021-07-17 22:28:55 +0600

    Create student_login.blade.php

---
+++ Code/Practice/student_login.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from www.urbanui.com/salt/jquery/pages/samples/login-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 13 Dec 2017 12:33:56 GMT -->
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Salt Admin</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../../node_modules/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../../node_modules/perfect-scrollbar/dist/css/perfect-scrollbar.min.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <link rel="stylesheet" href="../../node_modules/font-awesome/css/font-awesome.min.css" />
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../../css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../../images/favicon.html" />
+</head>
+<body class="sidebar-dark">
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper">
+      <div class="row">
+        <div class="content-wrapper full-page-wrapper auth-pages login-2">
+          <div class="card col-lg-4">
+            <div class="card-body px-5 py-5">
+              <div class="wrapper w-100">
+                <h3 class="card-title text-left mb-3">Login</h3>
+              </div>
+</body>
+
+
+</html>
+
+
+
+    
