b25b0a38858d946d2dc0b0aac2d364b775ff980f
mohsinkamal001 <mohsin.chowdhury01@northsouth.edu>
2021-07-24 00:13:19 +0600

    updating all files

--- Code/Project Code/Week 05/Wrong message code/Failed_php.php
+++
@@ -1,8 +0,0 @@
-<?php
-                $exception=Session::get('exception');
-
-                if ($exception){
-                  echo $exception;
-                  Session::put('exception',null);
-                }
-                ?> 
