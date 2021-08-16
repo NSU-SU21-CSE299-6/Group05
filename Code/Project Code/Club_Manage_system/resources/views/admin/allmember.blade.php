9ecd3669364747084ce0b150f0fa60e5eefc8eb0
mohsinkamal001 <mohsin.chowdhury01@northsouth.edu>
2021-08-16 10:31:32 +0600

    Frontend #46 : Design member info showing table with "view", "edit", "delete" button

--- Code/Project Code/Club_Manage_system/resources/views/admin/allmember.blade.php
+++ Code/Project Code/Club_Manage_system/resources/views/admin/allmember.blade.php
@@ -1,7 +1,43 @@
 @extends('layout')
 @section('content')
-
-allmember
+ 
+<div class="card">
+    <div class="card-body">
+         <h2 class="card-title">Data table</h2>
+            <div class="row">
+                <div class="col-12">
+                  <table id="order-listing" class="table table-striped" style="width:100%;">
+                    <thead>
+                      <tr>
+                          <th>ID</th>
+                          <th>Name</th>
+                          <th>Phone</th>
+                          <th>Image</th>
+                          <th>Address</th>
+                          <th>Club</th>
+                          <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                          <td>1</td>
+                          <td>Mohsin</td>
+                          <td>0181464656</td>
+                          <td>Manos</td>
+                          <td>Chittagong</td>
+                          <td>NSU ACM</td>
+                          <td>
+                            <button class="btn btn-outline-primary">View</button>
+                            <button class="btn btn-outline-warning">Edit</button>
+                            <button class="btn btn-outline-danger">Delete</button>
+                          </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
