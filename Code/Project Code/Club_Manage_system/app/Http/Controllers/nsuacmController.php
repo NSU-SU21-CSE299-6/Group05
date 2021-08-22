705c4fbbbfe4b7f466c864ee4337a1e12961c034
1821564642 Arkay Saha <arkay.saha@northsouth.edu>
2021-08-23 02:01:30 +0600

    Backend #84 Club Page Layout
    Backend part of club- 'NSU ACM' page

--- Code/Project Code/Club_Manage_system/app/Http/Controllers/nsuacmController.php
+++ Code/Project Code/Club_Manage_system/app/Http/Controllers/nsuacmController.php
@@ -13,6 +13,17 @@ class nsuacmController extends Controller
 {
     //
     public function nsuacm(){
+        $nsuacmmember_info = DB::table('members_tbl')
+                           ->where(['member_club'=>1])
+                           -> get();
+    
+    
+
+
+        $manage_member = view('admin.nsuacm')
+                        -> with('nsuacm_member_info',$nsuacmmember_info);
+        return view('layout')
+                    -> with('nsuacm',$manage_member);
         return view('admin.nsuacm');
     }
 }
