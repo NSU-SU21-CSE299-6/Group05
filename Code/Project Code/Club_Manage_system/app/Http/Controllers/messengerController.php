989a03a66f139b1aa446ed6ca41faf4a70b8f4c0
mohsinkamal001 <mohsin.chowdhury01@northsouth.edu>
2021-08-23 01:37:41 +0600

    Route #7 : message route added

---
+++ Code/Project Code/Club_Manage_system/app/Http/Controllers/messengerController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use APP\Http\Requests;
+use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redirect;
+use DB;
+use Session;
+session_start();
+
+class messengerController extends Controller
+{
+    // view messeger_page
+    public function messenger(){
+        return view('Message');
+    }
+}
