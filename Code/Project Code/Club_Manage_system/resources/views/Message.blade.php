c86c485e8d1e7e25d95bb3b99454886a5afd762a
mohsinkamal001 <mohsin.chowdhury01@northsouth.edu>
2021-08-23 01:49:14 +0600

    Frontend #7 : Chat list
    Here it show recent messages comes from other members

---
+++ Code/Project Code/Club_Manage_system/resources/views/Message.blade.php
@@ -0,0 +1,71 @@
+@extends('layout')
+@section('content')
+<div class="col-12">
+                    <div class="online-status d-flex justify-content-between align-items-center">
+                      <p class="chat">Chats</p>
+                      <span class="status offline online"></span>
+                    </div>
+                  </div>
+                  <ul class="profile-list">
+                      <li class="profile-list-item">
+                          <a href="#">
+                              <span class="pro-pic"><img src="http://via.placeholder.com/47x47" alt=""></span>
+                              <div class="user">
+                                  <p class="u-name">David</p>
+                                  <p class="u-designation">Python Developer</p>
+                              </div>
+                          </a>
+                      </li>
+                      <!-- list person -->
+                      <li class="profile-list-item">
+                          <a href="#">
+                              <span class="pro-pic"><img src="http://via.placeholder.com/47x47" alt=""></span>
+                              <div class="user">
+                                  <p class="u-name">Stella Johnson</p>
+                                  <p class="u-designation">SEO Expert</p>
+                              </div>
+                          </a>
+                      </li>
+                      <!-- list person -->
+                      <li class="profile-list-item">
+                          <a href="#">
+                              <span class="pro-pic"><img src="http://via.placeholder.com/47x47" alt=""></span>
+                              <div class="user">
+                                  <p class="u-name">Marina Michel</p>
+                                  <p class="u-designation">Business Development</p>
+                              </div>
+                          </a>
+                      </li>
+                      <!-- list person -->
+                      <li class="profile-list-item">
+                          <a href="#">
+                              <span class="pro-pic"><img src="http://via.placeholder.com/47x47" alt=""></span>
+                              <div class="user">
+                                  <p class="u-name">John Doe</p>
+                                  <p class="u-designation">Business Analyst</p>
+                              </div>
+                          </a>
+                      </li>
+                      <!-- list person -->
+                      <li class="profile-list-item">
+                          <a href="#">
+                              <span class="pro-pic"><img src="http://via.placeholder.com/47x47" alt=""></span>
+                              <div class="user">
+                                  <p class="u-name">Catherine</p>
+                                  <p class="u-designation">Tester</p>
+                              </div>
+                          </a>
+                      </li>
+                      <!-- list person -->
+                      <li class="profile-list-item">
+                          <a href="#">
+                              <span class="pro-pic"><img src="http://via.placeholder.com/47x47" alt=""></span>
+                              <div class="user">
+                                  <p class="u-name">Tim</p>
+                                  <p class="u-designation">Admin</p>
+                              </div>
+                          </a>
+                      </li>
+                  </ul>
+
+@endsection
